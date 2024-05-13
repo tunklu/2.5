2a0dc5eb9acce0052c1caabf5ebcdb2136145c8a
Denis <vin_ogr@list.ru>
2024-05-13 11:44:06 +0700

    Задание 3

---
+++ task3.py
@@ -0,0 +1,10 @@
+card_list = [3070, 2060, 3090, 3070, 3090]
+card_quantity = len(card_list)
+
+
+print('Количество видеокарт:', card_quantity)
+
+for i in range(0, card_quantity):
+    print('Видеокарта', i+1, card_list[i])
+
+card_listю
