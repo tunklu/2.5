6637e8fb17ba09b8199bbb44e4b402f7e11e079c
Denis <vin_ogr@list.ru>
2024-05-14 11:05:54 +0700

    Задание 3

--- task3.py
+++ task3.py
@@ -1,10 +1,19 @@
+from heapq import nlargest
+
+
 card_list = [3070, 2060, 3090, 3070, 3090]
 card_quantity = len(card_list)
-
+max_count = 0
 
 print('Количество видеокарт:', card_quantity)
 
 for i in range(0, card_quantity):
     print('Видеокарта', i+1, card_list[i])
 
-card_listю
+max_ = (max(card_list))
+max_count = card_list.count(max_)
+
+for i in range(0, max_count):
+    card_list.remove(max_)
+
+print(card_list)
