7e096ff1e1f55f47a7635dd394bdaa3e889a9960
Denis <vin_ogr@list.ru>
2024-05-30 13:09:05 +0700

    Задание 6 Done

--- task6.py
+++ task6.py
@@ -1,9 +1,10 @@
 def shift_list(steps, list_in):
     for i in range(0, steps):
-      list_in = list_in.insert(0, list_in.pop())
+      list_in.insert(0, list_in.pop(-1))
     return list_in
 
 list_ = [1, 2, 3, 4, 5]
 k = 1
+
 
 print(shift_list(k, list_))
